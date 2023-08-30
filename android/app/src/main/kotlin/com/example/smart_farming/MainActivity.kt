2ca3df226a0252c906eff5bbb2252a4a0d562d95
s4rath <sarathj810@gmail.com>
2023-08-30 10:33:12 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/smart_farming/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.smart_farming
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+      
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+ 
+
+
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+       
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+       
+        title: const Text("Smart Farming"),
+      ),
+      body: const Center(
+       
+        child: Column(
+         
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+            """ Login, 
+ HomePage, 
+ Classification Pages(Pest,Crop,Weed), 
+ IoT controling Pages.""",
+            ),
+           
+          ],
+        ),
+      ),
+      
+    );
+  }
+}
